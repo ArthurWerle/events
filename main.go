0b5bde78b960e7c728dda657a70ca03d7e1e51f9
arthurwerle <arthur.werle@gmail.com>
2026-06-14 15:53:01 -0300

    tests

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	connStr := os.Getenv("DATABASE_URL")
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close(context.Background())
+
+	rows, err := conn.Query(context.Background(), "SELECT id, payload, status FROM events")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var payload, status string
+		rows.Scan(&id, &payload, &status)
+		fmt.Println(id, payload, status)
+	}
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
